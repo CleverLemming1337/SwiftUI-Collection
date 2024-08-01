ba14d74e17103b86155606a1317a74585a61707c
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-08-01 13:39:50 +0200

    Initial Commit

---
+++ UICollection/UICollection.h
@@ -0,0 +1,18 @@
+//
+//  UICollection.h
+//  UICollection
+//
+//  Created by Leonard Fekete on 01.08.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UICollection.
+FOUNDATION_EXPORT double UICollectionVersionNumber;
+
+//! Project version string for UICollection.
+FOUNDATION_EXPORT const unsigned char UICollectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICollection/PublicHeader.h>
+
+
