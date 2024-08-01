ddd89136c7e2c3d47c2521ac2a4fce02a012bb47
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2024-08-01 13:50:19 +0200

    Add framework

---
+++ UICollection/UICollection.docc/README.md
@@ -0,0 +1,9 @@
+# SwiftUI Collection
+
+This is a little collection of useful SwiftUI components.
+
+![SwiftUI Card](Card.png)
+
+## How to use
+
+Just copy the the `SwiftUI Collection/Components.swift` file and leave a star if you like it ⭐️.
---
+++ UICollectionDemo/UICollectionDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  UICollectionDemoApp.swift
+//  UICollectionDemo
+//
+//  Created by Leonard Fekete on 01.08.24.
+//
+
+import SwiftUI
+
+@main
+struct UICollectionDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
